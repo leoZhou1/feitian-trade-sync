f3eee85c4180ee02301e6bc6f823d2980590bee2
leoZhou1 <zhouliye66@163.com>
2018-05-14 15:55:45 +0800

    代码初始化

---
+++ src/main/java/com/feitian/trade/sync/FeitianTradeSyncApplication.java
@@ -0,0 +1,12 @@
+package com.feitian.trade.sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeitianTradeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeitianTradeSyncApplication.class, args);
+    }
+}
